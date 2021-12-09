ddde779b0ed4b6b1463abe505b5fdb2bda524a97
Andrii Varlamov <andrii.varlamov@backendless.consulting>
2021-12-09 23:01:01 +0200

    [1.0.11] - install dependency during script

--- cmd.js
+++ cmd.js
@@ -1,75 +1,76 @@
 #!/usr/bin/env node
 
-const {existsSync, mkdirSync} = require('fs')
+const fs = require('fs')
 const path = require('path')
 const argv = require('yargs-parser')
 const cliPkg = require('./package')
 const generate = require('./generate')
 
 const template = {
-    dir: 'tropa-app',
-    main: './lib/index.js',
-    description: "Tropa App",
-    scripts: {
-        build: 'npx babel ./src --out-dir lib',
-        start: 'node ./lib/index.js --out-dir lib',
-        'start:prod': 'npx babel ./src --out-dir lib && node ./lib/index.js --out-dir lib',
-        debug: 'nodemon --exec babel-node ./src/index.js',
-        lint: 'eslint ./src --ext .js --fix',
-        test: 'mocha test',
-    },
-    dependencies: {
-        tropa: cliPkg.devDependencies.tropa,
-    },
-    devDependencies: {
-        "@babel/cli": cliPkg.devDependencies["@babel/cli"],
-        "@babel/core": cliPkg.devDependencies["@babel/core"],
-        "@babel/eslint-parser": cliPkg.devDependencies["@babel/eslint-parser"],
-        "@babel/node": cliPkg.devDependencies["@babel/node"],
-        "@babel/plugin-proposal-decorators": cliPkg.devDependencies["@babel/plugin-proposal-decorators"],
-        "@babel/plugin-transform-runtime": cliPkg.devDependencies["@babel/plugin-transform-runtime"],
-        "@babel/preset-env": cliPkg.devDependencies["@babel/preset-env"],
-        "@babel/register": cliPkg.devDependencies["@babel/register"],
-        "babel-plugin-parameter-decorator": cliPkg.devDependencies["babel-plugin-parameter-decorator"],
-        "eslint": cliPkg.devDependencies.eslint,
-        "nodemon": cliPkg.devDependencies.nodemon,
-        "mocha": cliPkg.devDependencies.mocha,
-    },
-    logInstructions: function (pkg) {
-        console.warn('saved package.json')
-        console.log(`project ${pkg.name} generated successfully`)
-        console.warn(`run 'npm install' to install the dependencies`)
-        console.warn(`run 'npm run build' to build the application`)
-        console.warn(`run 'npm run debug' to start the application in debug mode`)
-        console.warn(`run 'npm start' to start the application`)
-    }
+  dir: 'tropa-app',
+  main: './lib/index.js',
+  description: "Tropa App",
+  scripts: {
+    build: 'npx babel ./src --out-dir lib',
+    start: 'node ./lib/index.js --out-dir lib',
+    'start:prod': 'npx babel ./src --out-dir lib && node ./lib/index.js --out-dir lib',
+    debug: 'nodemon --exec babel-node ./src/index.js',
+    lint: 'eslint ./src --ext .js --fix',
+    test: 'mocha test',
+  },
+  dependencies: {
+    tropa: cliPkg.devDependencies.tropa,
+  },
+  devDependencies: {
+    "@babel/cli": cliPkg.devDependencies["@babel/cli"],
+    "@babel/core": cliPkg.devDependencies["@babel/core"],
+    "@babel/eslint-parser": cliPkg.devDependencies["@babel/eslint-parser"],
+    "@babel/node": cliPkg.devDependencies["@babel/node"],
+    "@babel/plugin-proposal-decorators": cliPkg.devDependencies["@babel/plugin-proposal-decorators"],
+    "@babel/plugin-transform-runtime": cliPkg.devDependencies["@babel/plugin-transform-runtime"],
+    "@babel/preset-env": cliPkg.devDependencies["@babel/preset-env"],
+    "@babel/register": cliPkg.devDependencies["@babel/register"],
+    "babel-plugin-parameter-decorator": cliPkg.devDependencies["babel-plugin-parameter-decorator"],
+    "eslint": cliPkg.devDependencies.eslint,
+    "nodemon": cliPkg.devDependencies.nodemon,
+    "mocha": cliPkg.devDependencies.mocha,
+  },
+  logInstructions: () => {
+    console.warn('saved package.json')
+    console.log(`tropa app generated successfully`)
+    console.warn(`run 'npm run start:prod' to build and run the application`)
+    console.warn(`run 'npm run build' to build the application`)
+    console.warn(`run 'npm start' to start the application`)
+    console.warn(`run 'npm run debug' to start the application in debug mode`)
+    console.warn(`run 'npm run test' to run tests`)
+    console.warn(`run 'npm run lint' to perform code analyse`)
+  }
 }
 
 const run = async args => {
-    const opts = argv(args)
-    const dir = opts._[0]
+  const opts = argv(args)
+  const dir = opts._[0]
 
-    if (dir && existsSync(dir)) {
-        if (dir !== '.' && dir !== './') {
-            throw new Error('directory ' + opts._[0] + ' already exists')
-        }
+  if (dir && fs.existsSync(dir)) {
+    if (dir !== '.' && dir !== './') {
+      throw new Error('directory ' + opts._[0] + ' already exists')
     }
-    if (dir === undefined) {
-        throw new Error('directory is not set')
-    }
+  }
+  if (dir === undefined) {
+    throw new Error('directory is not set')
+  }
 
-    mkdirSync(dir)
+  fs.mkdirSync(dir)
 
-    if (existsSync(path.join(dir, 'package.json'))) {
-        throw new Error('a package.json file already exists in target directory')
-    }
+  if (fs.existsSync(path.join(dir, 'package.json'))) {
+    throw new Error('a package.json file already exists in target directory')
+  }
 
-    return generate(dir, template)
+  return generate(dir, template)
 }
 
-run(process.argv.slice(2))
-    .catch(e => {
-        console.error(e.message)
+run(process.argv.slice(2)).catch(e => {
+  console.error(e.message)
 
-        process.exit(1)
-    })
+  process.exit(1)
+})
--- generate.js
+++ generate.js
@@ -1,50 +1,43 @@
 const generify = require("generify");
 const path = require("path");
-const {execSync} = require("child_process");
-const {readFile, writeFile} = require("fs");
+const childProcess = require("child_process");
+const fs = require("fs");
 
-module.exports = (targetDirectory, template) => {
-    return new Promise((resolve, reject) => {
-        generify(path.join(__dirname, template.dir), targetDirectory, {}, function (file) {
-            console.log(`generated ${file}`)
-        }, function (err) {
-            if (err) {
-                return reject(err)
-            }
+const onProjectGenerated = (dir, template) => {
+  process.chdir(dir)
 
-            process.chdir(targetDirectory)
-            execSync('npm init -y')
+  childProcess.execSync('npm init -y')
 
-            console.log(`reading package.json in ${targetDirectory}`)
-            readFile('package.json', (err, data) => {
-                if (err) {
-                    return reject(err)
-                }
+  console.log(`reading package.json in ${dir}`)
 
-                let pkg
-                try {
-                    pkg = JSON.parse(data)
-                } catch (err) {
-                    return reject(err)
-                }
+  const pkgJson = fs.readFileSync('package.json', 'utf8')
+  const pkg = JSON.parse(pkgJson)
 
-                pkg.name = template.dir
-                pkg.main = template.main
-                pkg.description = template.description
-                pkg.scripts = Object.assign(pkg.scripts || {}, template.scripts)
-                pkg.dependencies = Object.assign(pkg.dependencies || {}, template.dependencies)
-                pkg.devDependencies = Object.assign(pkg.devDependencies || {}, template.devDependencies)
+  pkg.name = template.dir
+  pkg.main = template.main
+  pkg.description = template.description
+  pkg.scripts = Object.assign(pkg.scripts || {}, template.scripts)
+  pkg.dependencies = Object.assign(pkg.dependencies || {}, template.dependencies)
+  pkg.devDependencies = Object.assign(pkg.devDependencies || {}, template.devDependencies)
 
-                console.log('edited package.json, saving')
-                writeFile('package.json', JSON.stringify(pkg, null, 2), (err) => {
-                    if (err) {
-                        return reject(err)
-                    }
+  console.log('edited package.json, saving')
 
-                    template.logInstructions(pkg)
-                    resolve()
-                })
-            })
-        })
-    })
+  fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2))
+
+  childProcess.execSync('npm install', {stdio: 'inherit'})
+
+  template.logInstructions()
 }
+
+module.exports = (dir, template) => new Promise((resolve, reject) => {
+  generify(path.join(__dirname, template.dir), dir, {}, file => console.log(`generated ${file}`), err => {
+    if (err) reject(err)
+
+    try {
+      onProjectGenerated(dir, template)
+      resolve()
+    } catch (error) {
+      reject(error)
+    }
+  })
+})
