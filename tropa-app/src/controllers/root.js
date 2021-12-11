b1bb8445242371105c8e0fe7f66f60294e54bebf
Andrii Varlamov <andrii.varlamov@backendless.consulting>
2021-12-11 10:26:45 +0200

    [1.0.12] - enrich app template

--- tropa-app/src/controllers/root.js
+++ tropa-app/src/controllers/root.js
@@ -1,9 +1,14 @@
-import { Get, Prefix } from 'tropa'
+import { Body, Get, Param, Post, Prefix, Query } from 'tropa'
 
 @Prefix('/')
-export default class Root {
+class Root {
   @Get('/')
   hello() {
     return { Hello: 'World' }
+  }
+
+  @Post('/{dynamicParam}')
+  echo(@Body() body, @Query() query, @Param() params) {
+    return { body, query, params }
   }
 }
